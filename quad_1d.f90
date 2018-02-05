4e563d781cb741388a69a9e2de0b8a422f5c3562
Wil Boshell <william.boshell@colorado.edu>
2018-02-05 16:47:10 -0700

    Created initial draft of quad_1d.f90

---
+++ quad_1d.f90
@@ -0,0 +1,6 @@
+module quad_1d
+  !This is an incomplete draft for now, I'll try to finish it in lab on 2/5.
+  implicit none
+  integer :: nvars, q
+  real, dimension(0:q,nvars)
+end module quad_1d
