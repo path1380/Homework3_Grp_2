c0bc3be4852f65395ec5e2a1cf5eace742e02e1e
Parth Thakkar <path1380@colorado.edu>
2018-02-06 17:21:40 -0700

    Quadrature_functions

--- InputControl.f90
+++ InputControl.f90
@@ -1,3 +1,10 @@
+!------------------------------------------------------------------------------------------------------------------------------------------------
+!
+!This module contains all the problem-specific information needed for the code to run. Users seeking to modify the problem can edit this file.
+!Function  f(x) defines the function to be plotted. Function q(i) defines the node distribution as a function of the element under consideration.
+!Subroutine domain(x0,xI,I) can be used to edit the domain under consideration (endpoints x0 and xI) and the number of elements desired(I).
+!
+!------------------------------------------------------------------------------------------------------------------------------------------------
 module InputControl
   use type_defs
   implicit none
@@ -5,11 +12,19 @@ contains
   real(dp) function f(x)
     implicit none
     real(dp) :: x
-    f = SIN(x)
+    f = x
   end function f
-  real(dp) function q(i)
+  integer function q(j)
     implicit none
-    real(dp) :: i
+    integer :: j
     q = 10 !Taking the largest degree of Legendre polynomials to be 10 for now
   end function q
+  subroutine domain(x0,xI,I)
+    implicit none
+    real(dp) :: x0,xI
+    integer :: I
+    x0=0.d0
+    xI=10.d0
+    I=10
+  end subroutine domain
 end module InputControl
