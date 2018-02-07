b694da415b340da71216ff23bc9ce74255033d98
Alex Rybchuk <olry2348@colorado.edu>
2018-02-07 16:06:42 -0700

    Aesthetic changes to test_leg

--- test_leg.f90
+++ test_leg.f90
@@ -1,8 +1,12 @@
 program test_leg
+! ================================================================
+! Create 2D array which contains the value of the first 5
+! Legendre polynomials between -1 and 1
+! ================================================================
     use type_defs
     use leg_funs
     implicit none
-    integer, parameter :: q = 4
+    integer, parameter :: q = 5
     real(dp) :: x
     real(dp), dimension(0:q) :: leg_arr
 
