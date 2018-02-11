75c839a1b4c4ce2fed96108e84d12461cde368d4
Parth Thakkar <path1380@colorado.edu>
2018-02-11 00:18:04 -0700

    report_pt_2

--- main.f90
+++ main.f90
@@ -17,21 +17,17 @@ program main
 
   implicit none
 
-  integer, parameter :: num_grdpts = 3, num_nodes = 15
+  integer, parameter :: num_grdpts = 11, num_nodes = 15
   integer :: degree_vec(num_grdpts - 1)
   real(dp) :: grdpts(num_grdpts), sample_nodes(num_nodes), function_vals(num_nodes)
   real(dp) :: lt_endpt, rt_endpt, stepsize
-  real(dp), dimension(num_grdpts -1) :: unif_err, L2_err
+  real(dp), dimension(num_grdpts -1) :: unif_err, L2_err,log_err
   type(quad_1d), dimension(1:num_grdpts-1) :: interval_info, approximation
   integer :: i, j
 
   !Grab grid information from InputControl
   call domain(grdpts)
   call legendre_degrees(degree_vec)
-
-  !allocate our quad_qd arrays
-  ! ALLOCATE(approximation(num_grdpts-1))
-  ! ALLOCATE(interval_info(num_grdpts-1))
 
   !Compute coefficients and approximation on each interval
   do i = 1,num_grdpts-1
@@ -66,15 +62,14 @@ program main
      approximation(i)%a(:,1) = approx_eval(lt_endpt,rt_endpt,num_nodes,sample_nodes,degree_vec(i),interval_info(i)%a(:,1))
      unif_err(i) = MAXVAL(ABS(approximation(i)%a(:,1) - function_vals))
      L2_err(i) = NORM2(approximation(i)%a(:,1) - function_vals)
+     log_err(i) = LOG(L2_err(i))
   end do
 
   !print errors out to terminal
-  write(*,'(2(E24.16))') maxval(unif_err), maxval(L2_err)
+  write(*,'(2(E24.16))') maxval(LOG(dble(degree_vec))), maxval(log_err)
 
   !Deallocate all used memory
   call delete_quad(num_grdpts-1, interval_info)
-  ! DEALLOCATE(interval_info)
   call delete_quad(num_grdpts-1, approximation)
-  ! DEALLOCATE(approximation)
 
 end program main
